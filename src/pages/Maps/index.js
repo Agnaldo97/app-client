82735d78e4e71835161abf156edb06eb054c573e
Agnaldo <agnaldo.filho@dasa.com.br>
2019-11-01 17:03:01 -0300

    Init geolocation

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,112 +1,126 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
-import MapView, { ProviderPropType } from 'react-native-maps';
-import MyLocationMapMarker from './MyLocationMapMarker';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Dimensions, Image, View, StatusBar, TouchableOpacity } from "react-native";
 
-const { width, height } = Dimensions.get('window');
+import MapView from 'react-native-maps';
 
-const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+import Polyline from '@mapbox/polyline';
+import Geolocation from '@react-native-community/geolocation';
 
-class Maps extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-      coordinate: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-      },
-      amount: 0,
-      enableHack: false,
-    };
-  }
 
-  increment() {
-    this.setState({ amount: this.state.amount + 10 });
-  }
+class LocationA extends Component {
+    constructor(props) {
+        super(props);
 
-  decrement() {
-    this.setState({ amount: this.state.amount - 10 });
-  }
+        this.state = {
+            latitude: null,
+            longitude: null,
+            error: null,
+            concat: null,
+            coords: [],
+            x: 'false',
+            cordLatitude: -22.906770,
+            cordLongitude: -43.220830,
+        };
 
-  toggleHack() {
-    this.setState({ enableHack: !this.state.enableHack });
-  }
+        this.mergeLot = this.mergeLot.bind(this);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          provider={this.props.provider}
-          style={styles.map}
-          initialRegion={this.state.region}
-        >
-          <MyLocationMapMarker
-            coordinate={this.state.coordinate}
-            heading={this.state.amount}
-            enableHack={this.state.enableHack}
-          />
-        </MapView>
-      </View>
-    );
-  }
+    }
+
+    componentDidMount() {
+        Geolocation.getCurrentPosition((position) => {
+            console.log('tt')
+            this.setState({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                error: null,
+            });
+            this.mergeLot();
+        },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 });
+    }
+
+    mergeLot() {
+        if (this.state.latitude != null && this.state.longitude != null) {
+            let concatLot = this.state.latitude + "," + this.state.longitude
+            this.setState({
+                concat: concatLot
+            }, () => {
+                this.getDirections(concatLot, "-6.270565,106.759550");
+            });
+        }
+
+    }
+
+    async getDirections(startLoc, destinationLoc) {
+
+        try {
+            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}`)
+            let respJson = await resp.json();
+            let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+            let coords = points.map((point, index) => {
+                return {
+                    latitude: point[0],
+                    longitude: point[1]
+                }
+            })
+            this.setState({ coords: coords })
+            this.setState({ x: "true" })
+            return coords
+        } catch (error) {
+            console.log('masuk fungsi')
+            this.setState({ x: "error" })
+            return error
+        }
+    }
+    render() {
+
+        return (
+            <MapView style={styles.map} initialRegion={{
+                latitude: -22.906770,
+                longitude: -43.220830,
+                latitudeDelta: 1,
+                longitudeDelta: 1
+            }}>
+
+                {!!this.state.latitude && !!this.state.longitude && <MapView.Marker
+                    coordinate={{ "latitude": this.state.latitude, "longitude": this.state.longitude }}
+                    title={"Your Location"}
+                />}
+
+                {!!this.state.cordLatitude && !!this.state.cordLongitude && <MapView.Marker
+                    coordinate={{ "latitude": this.state.cordLatitude, "longitude": this.state.cordLongitude }}
+                    title={"Your Destination"}
+                />}
+
+                {!!this.state.latitude && !!this.state.longitude && this.state.x == 'true' && <MapView.Polyline
+                    coordinates={this.state.coords}
+                    strokeWidth={2}
+                    strokeColor="red" />
+                }
+
+                {!!this.state.latitude && !!this.state.longitude && this.state.x == 'error' && <MapView.Polyline
+                    coordinates={[
+                        { latitude: this.state.latitude, longitude: this.state.longitude },
+                        { latitude: this.state.cordLatitude, longitude: this.state.cordLongitude },
+                    ]}
+                    strokeWidth={2}
+                    strokeColor="red" />
+                }
+            </MapView>
+        );
+    }
 }
 
-Maps.propTypes = {
-  provider: ProviderPropType,
-};
-
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  bubble: {
-    backgroundColor: 'rgba(255,255,255,0.7)',
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    borderRadius: 20,
-  },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
-  },
-  button: {
-    width: 80,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  hackButton: {
-    width: 200,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginVertical: 20,
-    backgroundColor: 'transparent',
-  },
-  toggleHack: { fontSize: 12, fontWeight: 'bold' },
-  ammountButton: { fontSize: 20, fontWeight: 'bold' },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
 });
 
-export default Maps;
+export default LocationA;
