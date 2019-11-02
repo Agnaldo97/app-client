71f1da7b4f90ba07faa9859203fa3efaf8609fd8
Agnaldo <agnaldo.filho@dasa.com.br>
2019-11-02 10:55:03 -0300

    Fix patient

--- src/pages/CPF/index.js
+++ src/pages/CPF/index.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
-
+import { Alert } from 'react-native';
+import api from '../../services/api';
+import AsyncStorage from '@react-native-community/async-storage';
 import { Container, Wrappe, Title, TButton, InputCpf } from './styles';
 
 export default function AudioInformation({ navigation }) {
     const [cpf, setCpf] = useState('');
     function handleSubmit() {
-        navigation.navigate('RecordAudio');
+        api.get('public/patient/' + cpf).then(response => {
+            AsyncStorage.setItem('patient:key', JSON.stringify(response.data.patient));
+            AsyncStorage.setItem('ccess-token:key', response.data.patient.accessToken);
+            navigation.navigate('RecordAudio');
+        }).catch((err) => {
+            Alert.alert('CPF inválido ou já em uso');
+        })
     }
 
     return (
@@ -13,17 +21,17 @@ export default function AudioInformation({ navigation }) {
             <Wrappe>
                 <Title>
                     Olá, vamos começar, para agilizar o atendimento, informe o
-                    CPF da criança.
+                    CPF do paciente.
                 </Title>
 
                 <InputCpf
-                    placeholder="Informe seu CPF"
+                    placeholder="Informe o CPF"
                     onSubmitEditing={handleSubmit}
                     values={cpf}
                     onChangeText={setCpf}
                 />
 
-                <TButton onPress={handleSubmit}>ENTENDI</TButton>
+                <TButton onPress={handleSubmit}>CONTINUAR</TButton>
             </Wrappe>
         </Container>
     );
